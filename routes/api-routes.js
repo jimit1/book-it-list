11c264a5e05ccf51fa39b6453f0920d2f6728fc3
jimMehta <jimit1@gmail.com>
2020-07-17 16:11:51 -0700

    merging

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const passport = require("../config/passport.js");
 const db = require("../models");
-
 const axios = require("axios");
 require("dotenv").config();
 
