9a9a39fcfc70ea9f102b7b92d69c85dc61043eff
Sosuke Brause <brosuke89@gmail.com>
2020-07-18 17:45:12 -0700

    list view template

--- client/assets/feed.js
+++ client/assets/feed.js
@@ -46,10 +46,10 @@ $(document).ready(function () {
       $("#listView").prepend(`
     
             <li>
-              <div class="collapsible-header"><i class="material-icons">filter_drama</i>${todo.title}</div>
-              <div class="collapsible-body">
-              <span>${todo.details}</span></div>
-            </li>        
+              <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+              <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+       
      
       `);
     });
@@ -97,3 +97,12 @@ $(document).ready(function () {
     });
   };
 });
+
+// <li>
+//   <div class="collapsible-header">
+//     <i class="material-icons">filter_drama</i>${todo.title}
+//   </div>
+//   <div class="collapsible-body">
+//     <span>${todo.details}</span>
+//   </div>
+// </li>;
