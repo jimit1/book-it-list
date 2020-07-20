b4d8a834c1df22657b81e390995bbbabd5490ebe
Mat Wilmot <matwilmot90@gmail.com>
2020-07-19 20:35:36 -0700

    changed spelling

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const bcryptjs = require("bcryptjs");
 
 //User
 module.exports = function (sequelize, DataTypes) {
-  const User = sequelize.define("User", {
+  const User = sequelize.define("user", {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
